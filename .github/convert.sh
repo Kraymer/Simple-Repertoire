3a40d1fc8aedfbb4cee4653ef8d052695169fe22
kraymer <kraymer@gmail.Com>
2021-01-24 22:26:02 +0100

    test

--- .github/convert.sh
+++ .github/convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+MUSESCORE="./build.release/mscore/mscore"
+
 repo_path=$1
 cd /github/workspace
 
@@ -13,6 +15,6 @@ do
   echo "file is " $file
   if [[ ${file} =~ \.(mscx)$ ]]; then
   	echo ${file}
-  	musescore --export-to out.png ${file}
+  	${MUSESCORE} --export-to out.png ${file}
   fi
 done
