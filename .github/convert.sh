d823f2a44a32c79ba16dacb8d6552272e4605745
kraymer <kraymer@gmail.Com>
2021-01-24 22:20:55 +0100

    test

--- .github/convert.sh
+++ .github/convert.sh
@@ -4,9 +4,11 @@ set -x
 repo_path=$1
 cd /github/workspace
 
-echo "Hello " "$@"
 
-for file in "$@"
+read -ra arr <<<"$@"
+for a in ; do echo "[$a]"; done
+
+for file in "${arr[@]}"
 do
   echo "file is " $file
   if [[ ${file} =~ \.(mscx)$ ]]; then
