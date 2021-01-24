a93572cbf904006ccb8a3961951fb34cfef90989
kraymer <kraymer@gmail.Com>
2021-01-24 21:55:22 +0100

    test

--- .github/convert.sh
+++ .github/convert.sh
@@ -2,12 +2,14 @@
 set -x
 
 repo_path=$1
+cd /home/runner/work/Simple-Repertoire/
 cd $repo_path
 
 echo "Hello " "$@"
 
 for file in "${@:2}"
 do
+  echo "file is " $file
   if [[ ${file} =~ \.(mscx)$ ]]; then
   	echo ${file}
   	musescore --export-to out.png ${file}
