701e3bdb5c7b9f1f3a2ef92ecc286df51eedb95c
Fabrice Laporte <fabrice@yescapa.com>
2021-01-25 22:12:40 +0100

    test

--- .github/convert.sh
+++ .github/convert.sh
@@ -14,8 +14,9 @@ for file in "${arr[@]}"
 do
 
   if [[ ${file} =~ \.(mscx)$ ]]; then
-    filedir=${file%/*}
-    filename=${file##*/}
+    filedir="${file%/*}"
+    filename="${file##*/}"
+    filename_noext="${filename%.*}"
   	${MUSESCORE} --export-to ${OUTPUT_DIR}/out.png ${file}
     cd ${OUTPUT_DIR}
     cmd="convert -append "
@@ -24,7 +25,7 @@ do
         convert ${png} -trim +repage trim-${png}
         cmd+=" spacer_260.png trim-${png}"
     done
-    cmd+=" ${filename}.png"
+    cmd+=" ${filename_noext}.png"
     $cmd
     ls /github/workspace/
     cp ${filename}.png /github/workspace/${filedir}
